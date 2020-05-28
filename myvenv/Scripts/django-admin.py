691ced346f757f0fa9d84fa3a9bec9f6efdc75a4
YujinKim-Sciedu <sandwich17yj@likelion.org>
2020-05-28 17:32:53 +0900

    20/05/28 : mtv session #4

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\yujin\visualcode-workspace\멋쟁이사자처럼\mtv0525\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
